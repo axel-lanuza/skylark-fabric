8291bc392cec18ffe26cd81235cd52049596b92b
hudaokeji <github@hudaokeji.com>
2020-03-30 07:50:50 +0900

    init commit

---
+++ dist/skylark-fabric/canvas2d.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-fabric - The skylark canvas2d utility library.
+ * @author Hudaokeji Co.,Ltd
+ * @version v0.9.0
+ * @link www.skylarkjs.org
+ * @license MIT
+ */
+define(["skylark-langx/skylark"],function(a){return a.attach("intg.fabric",{})});
+//# sourceMappingURL=sourcemaps/canvas2d.js.map
---
+++ src/main.js
@@ -0,0 +1,21 @@
+define([
+    "./canvas2d",
+    "./Canvas",
+    "./Circle",
+    "./Color",
+    "./Ellipse",
+    "./Group",
+    "./Image",
+    "./Line",
+    "./Object",
+    "./Path",
+    "./Point",
+    "./Polygon",
+    "./Polyline",
+    "./Rect",
+    "./Text",
+    "./Triangle",
+    "./util"
+], function(canvas2d) {
+    return canvas2d;
+});
